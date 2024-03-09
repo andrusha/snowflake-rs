56a71d753a419bdf9dd3a4d49ddf54d773139f13
andrusha <korzhuev@andrusha.me>
2024-03-09 16:45:50 +0100

    auth args from env support (#33)

--- snowflake-api/examples/polars/src/main.rs
+++ snowflake-api/examples/polars/src/main.rs
@@ -1,25 +1,14 @@
 use anyhow::Result;
 use polars::frame::DataFrame;
-use snowflake_api::{AuthArgs, AuthType, PasswordArgs, SnowflakeApiBuilder};
+
+use snowflake_api::SnowflakeApi;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv::dotenv().ok();
     tracing_subscriber::fmt::init();
 
-    let auth_args = AuthArgs {
-        account_identifier: std::env::var("SNOWFLAKE_ACCOUNT").expect("SNOWFLAKE_ACCOUNT not set"),
-        warehouse: std::env::var("SNOWLFLAKE_WAREHOUSE").ok(),
-        database: std::env::var("SNOWFLAKE_DATABASE").ok(),
-        schema: std::env::var("SNOWFLAKE_SCHEMA").ok(),
-        username: std::env::var("SNOWFLAKE_USER").expect("SNOWFLAKE_USER not set"),
-        role: std::env::var("SNOWFLAKE_ROLE").ok(),
-        auth_type: AuthType::Password(PasswordArgs {
-            password: std::env::var("SNOWFLAKE_PASSWORD").expect("SNOWFLAKE_PASSWORD not set"),
-        }),
-    };
-
-    let api = SnowflakeApiBuilder::new(auth_args).build()?;
+    let api = SnowflakeApi::from_env()?;
 
     // run a query that returns a tabular arrow response
     run_and_print(
@@ -42,7 +31,7 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn run_and_print(api: &snowflake_api::SnowflakeApi, sql: &str) -> Result<()> {
+async fn run_and_print(api: &SnowflakeApi, sql: &str) -> Result<()> {
     let res = api.exec_raw(sql).await?;
 
     let df = DataFrame::try_from(res)?;
