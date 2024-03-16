8a9b47b5f42bec3a03bf4a482f7afdb872362718
Andrew Korzhuev <korzhuev@andrusha.me>
2024-03-16 15:03:53 +0100

    Use auth env for file transfer example

--- snowflake-api/examples/filetransfer.rs
+++ snowflake-api/examples/filetransfer.rs
@@ -2,81 +2,24 @@ use anyhow::Result;
 use arrow::util::pretty::pretty_format_batches;
 use clap::Parser;
 use snowflake_api::{QueryResult, SnowflakeApi};
-use std::fs;
 
 extern crate snowflake_api;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Path to RSA PEM private key
-    #[arg(long)]
-    private_key: Option<String>,
-
-    /// Password if certificate is not present
-    #[arg(long)]
-    password: Option<String>,
-
-    /// <account_identifier> in Snowflake format, uppercase
-    #[arg(short, long)]
-    account_identifier: String,
-
-    /// Database name
-    #[arg(short, long)]
-    database: String,
-
-    /// Schema name
-    #[arg(long)]
-    schema: String,
-
-    /// Warehouse
-    #[arg(short, long)]
-    warehouse: String,
-
-    /// username to whom the private key belongs to
-    #[arg(short, long)]
-    username: String,
-
-    /// role which user will assume
-    #[arg(short, long)]
-    role: String,
-
     #[arg(long)]
     csv_path: String,
 }
 
+// SNOWFLAKE_ACCOUNT=... SNOWFLAKE_USER=... SNOWFLAKE_PASSWORD=... SNOWFLAKE_DATABASE=... SNOWFLAKE_SCHEMA=... \
+//   cargo run --example filetransfer --  --csv-path $(pwd)/examples/oscar_age_male.csv
 #[tokio::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init();
 
     let args = Args::parse();
-
-    let mut api = match (&args.private_key, &args.password) {
-        (Some(pkey), None) => {
-            let pem = fs::read_to_string(pkey)?;
-            SnowflakeApi::with_certificate_auth(
-                &args.account_identifier,
-                Some(&args.warehouse),
-                Some(&args.database),
-                Some(&args.schema),
-                &args.username,
-                Some(&args.role),
-                &pem,
-            )?
-        }
-        (None, Some(pwd)) => SnowflakeApi::with_password_auth(
-            &args.account_identifier,
-            Some(&args.warehouse),
-            Some(&args.database),
-            Some(&args.schema),
-            &args.username,
-            Some(&args.role),
-            pwd,
-        )?,
-        _ => {
-            panic!("Either private key path or password must be set")
-        }
-    };
+    let mut api = SnowflakeApi::from_env()?;
 
     log::info!("Creating table");
     api.exec(
